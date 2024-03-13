60728b3e235a18bda3c97a074515e0e8f30e818b
Alessandro de Almeida <agostini55@gmail.com>
2024-03-13 13:34:11 -0300

    Update urls.py

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('', include('myapp.urls')),
 ]
 
 # add at the last
