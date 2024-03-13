53cb98cfde2f768e4029ee1c745e1b63ebf217f2
Alessandro de Almeida <agostini55@gmail.com>
2024-03-13 13:39:10 -0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -11,14 +11,14 @@ def create_view(request):
         return redirect('read')
     # Se o método for GET, exibe a página com o formulário de criação
     elif request.method == "GET":
-        return render(request, 'templates/myapp/create_template.html')
+        return render(request, 'myapp/create_template.html')
     # Se o método HTTP não for nem GET nem POST, retorna um erro 405
     else:
         return HttpResponse('Método HTTP não permitido', status=405)
 
 def read_view(request):
     objects = MyModel.objects.all()
-    return render(request, 'templates/myapp/read_template.html', {'objects': objects})
+    return render(request, 'myapp/read_template.html', {'objects': objects})
 
 def update_view(request, pk):
     if request.method == "POST":
